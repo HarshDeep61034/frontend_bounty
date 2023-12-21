295f6cef684ebe5733cc56d8cf0a1a53c14352f8
Harsh Deep <hdeep61034@gmail.com>
2023-12-21 23:13:16 +0530

    Project Done

--- my-project/src/components/Chat.jsx
+++ my-project/src/components/Chat.jsx
@@ -1,10 +1,12 @@
+import React, { useState } from "react";
 import Comment from "./Comment";
 import data from "../../que.json";
+
 const Chat = () => {
-  const questions = data.questions;
+  const initialQuestions = data.questions;
+  const [questions, setQuestions] = useState(initialQuestions);
 
   const sortedQuestions = [...questions].sort((a, b) => b.upvotes - a.upvotes);
-
   const top3elements = sortedQuestions
     .slice(0, 3)
     .map((item) => (
@@ -29,26 +31,45 @@ const Chat = () => {
       />
     ));
 
-  const elements = questions.map((item) => (
-    <Comment
-      key={item.id} // Adding a key is essential when mapping over an array in React
-      text={item.text}
-      upvotes={item.upvotes}
-      downvotes={item.downvotes}
-      user={item.user}
-    />
-  ));
+  const addQuestion = (text) => {
+    const newQuestion = {
+      id: questions.length + 1,
+      text: text,
+      upvotes: 0,
+      downvotes: 0,
+      user: "NewUser",
+    };
+    setQuestions((prevQuestions) => [...prevQuestions, newQuestion]);
+  };
+
+  const handleInputKeyPress = (event) => {
+    if (event.key === "Enter") {
+      const inputElement = event.target;
+      addQuestion(inputElement.value);
+      inputElement.value = "";
+    }
+  };
+
   return (
     <div className="w-[100vw] h-[100vh] bg-black flex flex-wrap">
       <div className="w-[450px] relative h-[700px] mx-3">
-        <div className=" m-4 w-[450px] h-[600px] bg-[#111827] text-white p-2 rounded-md overflow-scroll">
-          <h1 className="text-center">QnA</h1>
-          {elements}
+        <div className="m-4 w-[450px] h-[600px] bg-[#111827] text-white p-2 rounded-md overflow-scroll">
+          <h1 className="text-center"></h1>
+          {questions.map((item) => (
+            <Comment
+              key={item.id}
+              text={item.text}
+              upvotes={item.upvotes}
+              downvotes={item.downvotes}
+              user={item.user}
+            />
+          ))}
         </div>
         <input
           type="text"
           name="input"
           className="w-full rounded-md mx-2 p-1 text-lg absolute bottom-2"
+          onKeyDown={handleInputKeyPress}
         />
       </div>
       <div className="m-4 w-[450px] h-[600px] bg-[#111827] p-2 rounded-md relative text-white">
